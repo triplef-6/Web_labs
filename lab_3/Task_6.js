228c81ab00739c9c29b967fea5a6157071dda8c5
yura <128141784+triplef-6@users.noreply.github.com>
2024-09-18 10:50:35 +0600

    Лаб 3 ALL

--- lab_3/Task_6.js
+++ lab_3/Task_6.js
@@ -10,6 +10,10 @@ let n2 = randomInteger(1, 100);
 console.log("Число1:",  n1);
 console.log("Число2:",  n2);
 
+// let a1 = 27;
+// let a2 = 38;
+// console.log(a1 + a2);
+
 console.log("Остаток от деления первого числа на второе:", n1 % n2);
 console.log("Сумма чисел:", n1 + n2);
 console.log("Произведение чисел:",  n1 * n2);
--- lab_3/Task_7.js
+++ lab_3/Task_7.js
@@ -8,11 +8,13 @@ console.log("Задание 7");
 let n1 = randomInteger(1, 100);
 let n2 = randomInteger(1, 100);
 let n3 = randomInteger(1, 100);
+let n4 = randomInteger(1, 100);
 console.log("Число1:", n1);
 console.log("Число2:", n2);
 console.log("Число3:", n3);
+console.log("Число4:", n4);
 
-console.log("Среднее арифметическое:",  (n1 + n2 + n3) / 3);
-console.log("Сумма квадратов:",  n1**2 + n2**2 + n3**2);
-console.log("Наибольшее число:",  Math.max(n1, n2, n3));
-console.log("Наименьшее число:",  Math.min(n1, n2, n3));
+console.log("Среднее арифметическое:",  (n1 + n2 + n3 + n4) / 4);
+console.log("Сумма квадратов:",  n1**2 + n2**2 + n3**2 + n4**2);
+console.log("Наибольшее число:",  Math.max(n1, n2, n3, n4));
+console.log("Наименьшее число:",  Math.min(n1, n2, n3, n4));
