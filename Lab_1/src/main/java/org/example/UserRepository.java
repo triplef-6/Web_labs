d3b1c33e55d4b5175081d808c9dabe04ec509e4d
yura <128141784+triplef-6@users.noreply.github.com>
2025-02-18 13:49:02 +0600

    Лаб 1 добавленны методы delete update userById

--- Lab_1/src/main/java/org/example/UserRepository.java
+++ Lab_1/src/main/java/org/example/UserRepository.java
@@ -16,10 +16,40 @@ public class UserRepository {
         return Collections.unmodifiableList(users);
     }
 
+    public User getUserById(int id) {
+        for (User u : users) {
+            if (u.getId() == id) {
+                return u;
+            }
+        }
+
+        return null;
+    }
+
     public User create(User user) {
         User newUser = new User((int) getNextID(), user.getName(), user.getSurname());
         users.add(newUser);
         return newUser;
+    }
+
+    public User update(User user) {
+        for (User u : users) {
+            if (u.getId() == user.getId()) {
+                u = new User(user.getId(), user.getName(), user.getSurname());
+                return u;
+            }
+        }
+
+        return null;
+    }
+
+    public void delete(int id) {
+        for (User u : users) {
+            if (u.getId() == id) {
+                users.remove(u);
+                return;
+            }
+        }
     }
     // дополните
     private long getNextID(){
--- Lab_1/src/main/java/org/example/controllers/UserController.java
+++ Lab_1/src/main/java/org/example/controllers/UserController.java
@@ -28,6 +28,12 @@ public class UserController {
         return userRepository.getAllUser();
     }
 
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public User userById(@RequestBody int id) {
+        return userRepository.getUserById(id);
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity<User> create(@RequestBody User newUser) {
@@ -38,4 +44,19 @@ public class UserController {
         return ResponseEntity.created(location).body(createdUser);
     }
 
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseBody
+    public ResponseEntity<User> update(@RequestBody User user) {
+        User updateUser = userRepository.update(user);
+        URI location = UriComponentsBuilder.fromPath("/user/")
+                .path(String.valueOf(updateUser.getId()))
+                .build().toUri();
+        return ResponseEntity.created(location).body(updateUser);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    @ResponseBody
+    public void delete(@RequestBody int id) {
+        userRepository.delete(id);
+    }
 }
